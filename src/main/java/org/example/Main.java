76094058b8a3486efefdf3001c4fe4cb4b934537
Antsg9725 <1581487@alu.murciaeduca.es>
2024-03-07 22:25:22 +0100

    Subimos el nuevo repositorio

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -12,6 +12,13 @@ public class Main {
         // Crear una nueva agenda.
 
         Agenda agendaContactos = new Agenda();
+
+        // Añadir contactos a la agenda.
+        agendaContactos.addContact("John Doe", "1234567890");
+        agendaContactos.addContact("Jane Doe", "9876543210");
+        agendaContactos.addContact("Mary Jane", "1122334455");
+
+
         // Imprimir todos los contactos de la agenda.
         System.out.println("Todos los contactos: ");
         for (Persona c : agendaContactos.getContacts()) {
@@ -37,6 +44,26 @@ public class Main {
         agendaContactos.addContact("John Doe", "1234567890");
         agendaContactos.addContact("Jane Doe", "9876543210");
         agendaContactos.addContact("Mary Jane", "1122334455");
+
+        // Imprimir todos los contactos de la agenda.
+        System.out.println("Todos los contactos: ");
+        for (Persona c : agendaContactos.getContacts()) {
+            System.out.println(c.getName() + " -> " + c.getPhones());
+        }
+
+        // Añadir un nuevo teléfono a un contacto existente.
+        agendaContactos.addContact("John Doe", "6543219876");
+
+        // Eliminar un contacto de la agenda.
+        agendaContactos.removeContact("Jane Doe");
+
+        // Modificar el teléfono de un contacto existente en la agenda.
+        agendaContactos.modifyPhoneNumber("John Doe", "1234567890", "6666666666");
+
+        System.out.println("\nDespués de añadir, eliminar y modificar contactos: ");
+        for (Persona c : agendaContactos.getContacts()) {
+            System.out.println(c.getName() + " -> " + c.getPhones());
+        }
 
     }
 
